25af8ce33c1ce189c796411fb8959a2689995145
saulamsal <saulamsal@gmail.com>
2021-04-25 12:28:20 -0500

    setup the env variables

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+class config
+{
+    function __construct()
+    {
+        $this->host = "localhost";
+        $this->user  = "root";
+        $this->pass = "welcome";
+        $this->db = "mydb13";
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+
+$dotenv =  Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
